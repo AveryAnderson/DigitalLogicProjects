9fb7e76401fdae24bea3603abc0f810e1ded8166
Jolo9058 <32269983+Jolo9058@users.noreply.github.com>
2017-10-10 19:05:47 -0600

    Update Multiplexer.v

--- ProjectOne/Multiplexer.v
+++ ProjectOne/Multiplexer.v
@@ -1,1 +1,36 @@
+module Mux(A, B, C, D, Select, Out);
 
+input [3:0] A, B, C, D;
+input [1:0] Select;
+output[3:0] Out;
+reg [3:0] rOut;
+
+always @(Select)
+begin
+if(Select[0] == 0)
+	begin
+		if(Select[1] == 0)
+			begin
+				 rOut = A;
+			end
+		else
+			begin
+				 rOut = B;
+			end
+	end
+else
+	begin
+	if(Select[1] == 0)
+			begin
+				 rOut = C;
+			end
+		else
+			begin
+				 rOut = D;
+			end
+	end
+end	
+assign Out = rOut;
+
+
+endmodule
