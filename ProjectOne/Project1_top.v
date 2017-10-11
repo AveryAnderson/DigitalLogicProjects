73302d6db5ddb230a28bbd90c6aacdaa9ea63659
Jolo9058 <32269983+Jolo9058@users.noreply.github.com>
2017-10-10 19:35:52 -0600

    Update Project1_top.v

--- ProjectOne/Project1_top.v
+++ ProjectOne/Project1_top.v
@@ -1,1 +1,25 @@
+module Project1_Top(Button, Switch, Clock);
 
+input [1:0] Button;
+input [3:0] Switch;
+input Clock;
+
+reg slow = 0;
+reg [4:0]  counter = 0;
+
+always @(posedge Clock)
+begin
+	counter <= counter + 1;
+	if ( counter == 20)
+		begin
+			counter <= 0;
+			slow <= ~slow;
+		end
+end
+
+always @( Button or Switch)
+begin
+
+
+
+end
